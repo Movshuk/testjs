d81c6c0dc433114d625eb5e3b34627d47e22e7a9
Slava <pochta.mvn@mail.ru>
2019-05-30 11:52:05 +0300

    fist commit

---
+++ script/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+//alert("Hello");
+// var a = 5;
+// var b = 10;
+// var c = a * b;
+// console.log('=>' + (a+b));
+
+// let
+var number = 5;
+var string = "Text";
+var boolean = true;
+var obj = {
+    name: "Neo",
+    secondname: "Too"
+};
+
+// console.log(obj.name + " " + obj.secondname);
+// console.log(obj);
+
+// let  arr = [1, 2, 5, 6, 8, 0];
+
+// for (var i=0; i < arr.length; i++){
+//   console.log(arr[i]);
+// }
+
+var answer = confirm("Yes?");
+console.log(answer);
+console.log(typeof(answer));
+
+console.log("2" === 2);
