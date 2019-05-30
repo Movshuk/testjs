7259d7c9ae383baf42a4ed0438aa49ae83a9dd87
Slava <pochta.mvn@mail.ru>
2019-05-30 14:25:17 +0300

    callback function

--- script/script.js
+++ script/script.js
@@ -1,60 +1,12 @@
-'use strict';
-
-//alert("Hello");
-// var a = 5;
-// var b = 10;
-// var c = a * b;
-// console.log('=>' + (a+b));
-
-// let
-var number = 5;
-var string = "Text";
-var boolean = true;
-var obj = {
-    name: "Neo",
-    secondname: "Too"
-};
-
-// console.log(obj.name + " " + obj.secondname);
-// console.log(obj);
-
-// let  arr = [1, 2, 5, 6, 8, 0];
-
-// for (var i=0; i < arr.length; i++){
-//   console.log(arr[i]);
-// }
-
-var answer = confirm("Yes?");
-console.log(answer);
-console.log(typeof(answer));
-
-console.log("2" === 2);
-
-// ADD
-console.log(5%2);
-
-let num = 50;
-(num == 50) ? console.log("true") : console.log("false");
-
-
-// 
-for (var x = 0; x < 10; x++) {
+function foo(x, callback){
   console.log(x);
-}
-console.log("end > " + x);
-
-//
-
-function foo(x, y){
-  console.log(x + y);
+  callback();
 }
 
-foo(5,2);
-
-let z = function(a, b){
-  return a + b;
+function boo(){
+  console.log(2);
 }
 
-console.log(z(1, 1));
+foo(1, boo);
 
-
+boo();
